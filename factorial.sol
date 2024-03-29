a2c35edfa7055d14b3ec9147098d69911dce028a
N. Tapaswi <110029714+2100031010@users.noreply.github.com>
2024-03-29 11:47:18 +0530

    Update factorial.sol

--- factorial.sol
+++ factorial.sol
@@ -1,1 +1,13 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+contract Factorial {
+    function calculateFactorial(uint256 number) public pure returns (uint256) {
+        uint256 result = 1;
+        for (uint256 i = 2; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
